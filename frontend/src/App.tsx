848d3f2a008f130288cc96db18ba07047883608f
Andre Araujo <and.araujos@gmail.com>
2022-01-12 10:22:02 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,29 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Listing from 'pages/listing';
+import Form from 'pages/form';
+import Navbar from "components/navbar";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
+
+/*
 import React from 'react';
 import Navbar from './components/navbar';
 
@@ -10,3 +36,5 @@ function App() {
 }
 
 export default App;
+
+*/
